4bb3ca0a7fe500558a90628b688992c268f305f5
the-moezzat <dev.moezzat@gmail.com>
2023-05-15 22:58:12 +0300

    feat: Add card that show character data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import {CharacterCard} from "./components/characterCard/CharacterCard";
+
 function App() {
   return (
     <>
       <div>
-          Hello world!
+          <CharacterCard name={"Rick"} gender={"male"} id={1} imageUrl={"https://rickandmortyapi.com/api/character/avatar/180.jpeg"} spices={"human"} status={"alive"}/>
       </div>
     </>
   )
---
+++ src/components/characterCard/CharacterCard.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {Button, Card, CardActions, CardContent, CardMedia, Stack, styled, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
+
+interface ICharacterCard {
+    id: number
+    name: string,
+    imageUrl: string,
+    spices: string,
+    gender: string,
+    status: string
+}
+
+const Img = styled("img")({
+    width: "150px", height: "100px", objectFit: "cover", borderRadius: "16px"
+});
+export const CharacterCard: React.FC<ICharacterCard> = ({id, gender, imageUrl, status, spices, name}) => {
+    return (
+        <Card
+            sx={{display: "flex", gap: "10px", backgroundColor: "transparent"}}
+            elevation={0}
+        >
+            <CardMedia>
+                <Img
+
+                    src={imageUrl}
+                    alt={name}
+                />
+            </CardMedia>
+            <CardContent sx={{padding: "0px"}}>
+                <Typography variant="h5" mb={"8px"}>
+                    {name}
+                </Typography>
+
+                <Stack direction="row" spacing={2} mb={"4px"}>
+                    <Typography paragraph mb={0}>
+                        Status: {status}
+                    </Typography>
+                    <Typography paragraph mb={0}>
+                        Gender: {gender}
+                    </Typography>
+                </Stack>
+                <Typography paragraph mb={0}>
+                    Species: {spices}
+                </Typography>
+            </CardContent>
+            <CardActions sx={{ml: "auto", alignSelf: "flex-start"}}>
+                <Link to={`/character/${id}`}>
+                    <Button variant="text" size="small" sx={{fontWeight: 'bold'}}>
+                        Explore
+                    </Button>
+                </Link>
+            </CardActions>
+        </Card>
+    );
+};
