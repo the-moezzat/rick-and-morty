a1dc8e8a8ebf12e3e1664036203a20b153743a3a
the-moezzat <dev.moezzat@gmail.com>
2023-05-15 20:16:28 +0300

    feat: Add react router path

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([{
+    path: '/',
+    element: <App/>,
+    children: [
+        {
+            path: "/character/:characterId",
+            element: ''
+        }
+    ]
+}])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
